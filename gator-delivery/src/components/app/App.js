642086d9a650cd9d6af66380c902856737c7afc3
Michael Klein <makattack333@ufl.edu>
2020-07-12 18:12:17 -0400

    Added Community Board Prototype

--- gator-delivery/src/components/app/App.js
+++ gator-delivery/src/components/app/App.js
@@ -1,12 +1,18 @@
 import React, {useState, useEffect} from 'react';
 import '../index.css';
 import Login from '../sign-in/Login'
+import Board from '../community/Board'
 
 function App() {
   return (
     <div className="overall-div">
       <div className="main-title">
         <h1>Gator Delivery</h1>
+          <div className="column-1 community">
+          <br></br>
+          <h3>Community Board</h3>
+          <Board/>
+          </div>
       </div>
       <div className="row-1 sign-in">
         <Login/>
