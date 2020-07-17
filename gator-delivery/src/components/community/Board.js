475955ec4d92983b65c10cecd38e22f4972ea638
Michael Klein <makattack333@ufl.edu>
2020-07-17 13:57:11 -0400

    New Commit

--- gator-delivery/src/components/community/Board.js
+++ gator-delivery/src/components/community/Board.js
@@ -6,17 +6,19 @@ const Board = (props) => {
     return(
         
         <section>
+            <section>
             <h3>Community Board</h3>
             <textbox classname="register ask">Ask a question</textbox>
-            <form action="" method="">
-                <br></br><textarea rows="5" cols="36"></textarea>
-            </form>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
             <button className="register ask">Submit</button>
-            <br></br><br></br><textbox classname="register answer">Answer a question</textbox>
-            <form action="" method="">
-                <br></br><textarea rows="5" cols="36"></textarea>
-            </form>
+            <br></br><textbox classname="register answer">Answer a question</textbox>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" cols="36"></textarea>
             <button className="register ask">Submit</button>
+
+            <h3>Community Posts</h3>
+            <input type="text" class="form-control" rows="7" placeholder="Post a question"></input>
+            <input type="text" class="form-control" placeholder="Last name"></input>
+        </section>
         </section>
     );
 };
