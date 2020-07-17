746dad566e7c775926dddd327d4c82abd0c9c57c
Michael Klein <makattack333@ufl.edu>
2020-07-17 17:06:55 -0400

    New Dropdowns

--- gator-delivery/src/components/community/Board.js
+++ gator-delivery/src/components/community/Board.js
@@ -30,21 +30,40 @@ const Board = (props) => {
         };
         axios.post('http://localhost:5000/posts/add', newPost);  
         
-        <section>
-            <section>
-            <h3>Community Board</h3>
-            <textbox classname="register ask">Ask a question</textbox>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            <button className="register ask">Submit</button>
-            <br></br><textbox classname="register answer">Answer a question</textbox>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" cols="36"></textarea>
-            <button className="register ask">Submit</button>
+        window.location = '/community';
+        setPost('');
+        
+    };
+    return(
+        <div>
+            <h1>
+                Community Board
+            </h1>
+            <h3>
+                Ask questions and get answers from experts in your area
+            </h3>          
 
-            <h3>Community Posts</h3>
-            <input type="text" class="form-control" rows="7" placeholder="Post a question"></input>
-            <input type="text" class="form-control" placeholder="Last name"></input>
-        </section>
-        </section>
-};
-}
+            <div className="card post-editor">
+                <div className="card-body">
+                    Ask a question
+                    <form onSubmit={createPost}>
+                        <div className="form-group">
+                            <textarea className="form-control post-editor-input"
+                                value={post}
+                                onChange={e => setPost(e.target.value)}    
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input type="submit" value="Submit Post" className="btn btn-primary" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            { PostList() }
+
+        </div>  
+    );
+    }
+
 export default Board;
