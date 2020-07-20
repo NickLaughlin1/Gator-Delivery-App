514a70b26b935aa56d2e092df8c6fb96390ff6c1
Michael Klein <makattack333@ufl.edu>
2020-07-20 17:24:53 -0400

    Added FAQ Section

--- gator-delivery/src/components/faq/faq.js
+++ gator-delivery/src/components/faq/faq.js
@@ -13,11 +13,11 @@ return(
         <h5>A: Yes, like a charm</h5>
     <br></br>
         <h3>Q: How do I become a volunteer if I'm already signed up?</h3>
-        <h5>A: If you locate the tab at the top of your screen that says "User Account" and select "Edit Profile", you can
-            update any personal information you need as well as change member type between Volunteer or Customer </h5>
+        <h5>A: If you go into your "<Link to={ROUTES.ACCOUNT}>Account</Link>", you can 
+        update any personal information you need as well as change member type between Volunteer or Customer </h5>
     <br></br>
         <h3>Q: Do you store all my information that goes into the sign-up?</h3>
-        <h5>A: No, the primary verification needed is simply your username for verification of a valid account</h5>
+        <h5>A: No, the only information that is stored is your username for verification of a valid account</h5>
     <br></br>
         <h3>Q: This is my first time using Home Order, are there any reviews I can see of current volunteers?</h3>
         <h5>A: You can actually check them out on the <Link to={ROUTES.REVIEWS}>reviews</Link> section </h5>
