2861be2cec27c644a72e9f27eee0efd56139df7b
Minato Sky Myers <minato.sky.myers@gmail.com>
2020-07-26 21:43:59 -0400

    Added dropdown for times and AM PM

--- gator-delivery/src/components/calendar/Calendar.js
+++ gator-delivery/src/components/calendar/Calendar.js
@@ -4,6 +4,18 @@ import 'react-calendar/dist/Calendar.css';
 
 const CalendarComponent = (props) => {
 	const [date, setDate] = useState(new Date());
+	
+	const [hr1, setHour1] = useState(12);
+	const [min1, setMin1] = useState(0);
+	const [sec1, setSec1] = useState(0);
+	
+	const [hr2, setHour2] = useState(12);
+	const [min2, setMin2] = useState(0);
+	const [sec2, setSec2] = useState(0);
+	
+	const [before, setBefore] = useState("AM");
+	const [after, setAfter] = useState("PM");
+	
 	const onChange = date => {
 		setDate(date)
 	};
@@ -12,7 +24,24 @@ const CalendarComponent = (props) => {
 	};*/
 	
 	const confirmChoice = () => {
-		console.log(date[0])
+		console.log(date);
+		var values = [hr1, min1, sec1, hr2, min2, sec2];
+		var pass = true;
+		for(var i = 0; i < values.length; i++){
+			console.log(values[i]);
+			if(Number(values[i]) === parseInt(Number(values[i]), 10)){
+				if(values[i] < 0 || values[i] > 12){
+					pass = false;
+					console.log("Error: input is out of bounds. Enter an integer in between 0 and 12");
+				}
+			}else{
+				pass = false;
+				console.log("Error: input is not an integer.");
+			}
+		}
+		
+		console.log(before)
+		console.log(after)
 	}
 	return(
 		<div>
@@ -48,13 +77,55 @@ const CalendarComponent = (props) => {
 					<div>
 						&nbsp;&nbsp;&nbsp;&nbsp;{date[0].toLocaleString().substring(0, date[0].toLocaleString().indexOf(","))}
 						&nbsp;&nbsp;--&nbsp;&nbsp;
-						{date[0].toLocaleString().substring(date[0].toLocaleString().indexOf(",") + 2, date[0].toLocaleString().length)}
-						&nbsp;to&nbsp; 
-						{date[1].toLocaleString().substring(date[1].toLocaleString().indexOf(",") + 2, date[1].toLocaleString().length)}
+						<input
+							style = {{width: 25}}
+							type = "numeric"
+							placeholder = "12"
+							onChange={e => setHour1(e.target.value)}
+						/>:
+						<input
+							style = {{width: 25}}
+							type = "numeric"
+							placeholder = "00"
+							onChange={e => setMin1(e.target.value)}
+						/>:
+						<input
+							style = {{width: 25}}
+							type = "numeric"
+							placeholder = "00"
+							onChange={e => setSec1(e.target.value)}
+						/>&nbsp;
+						<select id="inputAMPM1" onChange={e => setBefore(e.target.value)}>
+							<option defaultValue>AM</option>
+							<option value="PM">PM</option>
+						</select>&nbsp;to&nbsp;
+						<input
+							style = {{width: 25}}
+							type = "numeric"
+							placeholder = "11"
+							onChange={e => setHour2(e.target.value)}
+						/>:
+						<input
+							style = {{width: 25}}
+							type = "numeric"
+							placeholder = "59"
+							onChange={e => setMin2(e.target.value)}
+						/>:
+						<input
+							style = {{width: 25}}
+							type = "numeric"
+							placeholder = "59"
+							onChange={e => setSec2(e.target.value)}
+						/>&nbsp;
+						<select id="inputAMPM2" onChange={e => setAfter(e.target.value)}>
+							<option defaultValue>PM</option>
+							<option value="AM">AM</option>
+						</select>
 					</div>
 				</div>
 			) : (
 				/* range of dates */
+				/* add the dropdown boxes */
 				date[1] == null ? (
 					<div>
 						<div>
@@ -79,7 +150,28 @@ const CalendarComponent = (props) => {
 							<div>
 								&nbsp;&nbsp;&nbsp;&nbsp;{date[0].toLocaleString().substring(0, date[0].toLocaleString().indexOf(","))}
 								&nbsp;&nbsp;--&nbsp;&nbsp;
-								{date[0].toLocaleString().substring(date[0].toLocaleString().indexOf(",") + 2, date[0].toLocaleString().length)}
+								<input
+									style = {{width: 25}}
+									type = "numeric"
+									placeholder = "12"
+									onChange={e => setHour1(e.target.value)}
+								/>:
+								<input
+									style = {{width: 25}}
+									type = "numeric"
+									placeholder = "00"
+									onChange={e => setMin1(e.target.value)}
+								/>:
+								<input
+									style = {{width: 25}}
+									type = "numeric"
+									placeholder = "00"
+									onChange={e => setSec1(e.target.value)}
+								/>&nbsp;
+								<select id="inputAMPM3" onChange={e => setBefore(e.target.value)}>
+									<option defaultValue>AM</option>
+									<option value="PM">PM</option>
+								</select>
 							</div>
 							<div>
 								&nbsp;&nbsp;&nbsp;&nbsp;to
@@ -87,7 +179,28 @@ const CalendarComponent = (props) => {
 							<div>
 								&nbsp;&nbsp;&nbsp;&nbsp;{date[1].toLocaleString().substring(0, date[1].toLocaleString().indexOf(","))}
 								&nbsp;&nbsp;--&nbsp;&nbsp;
-								{date[1].toLocaleString().substring(date[1].toLocaleString().indexOf(",") + 2, date[1].toLocaleString().length)}
+								<input
+									style = {{width: 25}}
+									type = "numeric"
+									placeholder = "11"
+									onChange={e => setHour2(e.target.value)}
+								/>:
+								<input
+									style = {{width: 25}}
+									type = "numeric"
+									placeholder = "59"
+									onChange={e => setMin2(e.target.value)}
+								/>:
+								<input
+									style = {{width: 25}}
+									type = "numeric"
+									placeholder = "59"
+									onChange={e => setSec2(e.target.value)}
+								/>&nbsp;
+								<select id="inputAMPM4" onChange={e => setAfter(e.target.value)}>
+									<option defaultValue>PM</option>
+									<option value="AM">AM</option>
+								</select>
 							</div>
 						</div>
 						<div>
@@ -102,7 +215,7 @@ const CalendarComponent = (props) => {
 			<div>
 				&nbsp;
 			</div>
-			<div><button onClick = {confirmChoice}>Submit</button></div>
+			<div><button className="btn btn-primary" onClick = {confirmChoice}>Submit</button></div>
 		</div>
 	);
 };
