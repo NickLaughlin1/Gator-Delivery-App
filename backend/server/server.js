e10b5e65744ea71799ba07ce8a61aa14b7d9a9d8
beaubakken <beaubakken@ufl.edu>
2020-07-31 21:05:39 -0400

    trying to deploy

--- backend/server/server.js
+++ backend/server/server.js
@@ -7,11 +7,13 @@ import config from './config/config.js';
 import tasksRouter from './routes/tasksRouter.js';
 import postsRouter from './routes/postsRouter.js';
 
+const PORT = process.env.PORT || config.port;
+const URI = process.env.DB_URI || config.db.uri;
+
 //connect to database
-mongoose.connect(config.db.uri, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(() => {
+mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(() => {
     console.log(`Successfully connected to mongoose database.`)
 });
-
 
 //initialize app
 const app = express();
@@ -44,27 +46,17 @@ app.use('/', express.static('./../../client'));
 app.use('/tasks', tasksRouter);
 
 app.use('/posts', postsRouter);
-/* The next three middleware are important to the API that we are building */
 
-/* Request Handler for route /api/listings
-   TODO: Update the code to meet the required format - app.use('/api/listings', appropriateMiddleWare)
-   use the listings router middleware for requests to the api
-   check the variables list above
-*/
-//app.use('/api/listings/');
-
-
-/* Request Handler for coordinates
-   This is a server wrapper around Open Cage Data Geocoding API to get latitude + longitude coordinates from address */
-/*app.post('/api/coordinates', getCoordinates, (req, res) => {
-    res.send(req.results);
-});*/
-
-
-/* Request handler for all other routes
-   Sends a response (res) to go to the homepage for all routes not specified */
 app.all('/*', (req, res) => {
    res.sendFile(path.resolve('client/index.html'));
 });
 
-app.listen(config.port, () => console.log(`App now listening on port ${config.port}`));
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static('../../build'));
+
+   app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname, '../../build', 'index.html'));
+   });
+}
+
+app.listen(PORT, () => console.log(`App now listening on port ${PORT}`));
