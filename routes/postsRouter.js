8210a6153885d3af14af544fa42391f291335a02
ericahetrick <44039966+ericahetrick@users.noreply.github.com>
2020-08-07 18:12:46 -0400

    fixing files?

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -1,7 +1,11 @@
 import * as posts from '../controllers/postsController.js';
 import express from 'express'; //refers to Express the middleware helper for Node.js
+import {checkAuth} from '../authentication/authentication.js';
 const postsRouter = express.Router();
 
-postsRouter.post('/add', posts.create);
-postsRouter.get('/', posts.list);
+postsRouter.post('/add', checkAuth, posts.create);
+postsRouter.get('/:postId', checkAuth, posts.list);
+postsRouter.put('/', checkAuth, posts.update);
+postsRouter.get('/post/:postId', posts.read);
+postsRouter.delete('/:postId', checkAuth, posts.remove);
 export default postsRouter;
